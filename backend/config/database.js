d6df928a62eaf9627d7ce1b84ff09b82c6769729
Santosh H M <santhm18@in.ibm.com>
2022-07-09 19:53:22 +0530

    database config updated
    database config updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+// MONGO_URI= 'mongodb://127.0.0.1:27017/dev-db'
+// USER = "96d7eaead927ee"
+// PASSWORD = "682508c2ae42c5$5655"
 const { MONGO_URI } = process.env;
 const connection = MONGO_URI;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true})
